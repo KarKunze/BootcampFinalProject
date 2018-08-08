c4253cc2bca6dab259ce01e9bf72505253eddcb6
Karla Kunze <karlalkunze@gmail.com>
2018-08-08 19:06:50 -0400

    favicon

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -15,5 +15,9 @@
 <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro|Spectral" rel="stylesheet">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
 
+<link rel=icon href={{ asset('/images/walkinghorse.png') }}>
+<!-- favicon.ico -->
+
+
 <!-- Styles -->
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
