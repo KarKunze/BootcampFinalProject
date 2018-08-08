510ac28ab12ba3753004d0eea2ee018d8dbafc77
Karla Kunze <karlalkunze@gmail.com>
2018-08-08 09:38:05 -0400

    renamed images

--- resources/views/layouts/sidecolumn.blade.php
+++ resources/views/layouts/sidecolumn.blade.php
@@ -9,11 +9,11 @@
       Promoting our local arts, music, events, places, and businesses
     </div>
     <ul class="list-group list-group-flush">
-      <li class="list-group-item text-center"><img class="menu-image2" src="{{ asset('/images/bench.jpg') }}" height="100" width="120"><img class="menu-image" src="{{ asset('/storage/artists.jpg') }}" height="100" width="120"><a class="nav-link" href='/art'><strong>Art</strong></a></li>
-      <li class="list-group-item text-center"><img class="menu-image2" src="{{ asset('/storage/mmmb.jpg') }}"height="100" width="120"><img class="menu-image" src="{{ asset('/storage/bluegrass.jpg') }}"height="100" width="120"><a class="nav-link" href='/music'><strong>Music</strong></a></li>
-      <li class="list-group-item text-center"><img class="menu-image2" src="{{ asset('/storage/isaac.jpg') }}"height="100" width="120"><img class="menu-image" src="{{ asset('/storage/storywalk.jpg') }}" height="100" width="120"><a class="nav-link" href='/places'><strong>Places</strong></a></li>
-      <li class="list-group-item text-center"><img class="menu-image2" src="{{ asset('/storage/3rdst.jpg') }}" height="100" width="120"><img class="menu-image" src="{{ asset('/storage/local.jpg') }}"height="100" width="120"><a class="nav-link" href='/eatdrink'><strong>Eat & <br>Drink</strong></a></li>
-      <li class="list-group-item text-center"><img class="menu-image2" src="{{ asset('/storage/jazzymural.jpg') }}" height="100" width="120"><img class="menu-image" src="{{ asset('/storage/shakespeare.jpg') }}" height="100" width="120"><a class="nav-link" href='/local'><strong>Local<br>Business</strong></a></li>
+      <li class="list-group-item text-center"><img class="menu-image2" src="{{ asset('/images/bench.jpg') }}" height="100" width="120"><img class="menu-image" src="{{ asset('/images/artists.jpg') }}" height="100" width="120"><a class="nav-link" href='/art'><strong>Art</strong></a></li>
+      <li class="list-group-item text-center"><img class="menu-image2" src="{{ asset('/images/mmmb.jpg') }}"height="100" width="120"><img class="menu-image" src="{{ asset('/images/bluegrass.jpg') }}"height="100" width="120"><a class="nav-link" href='/music'><strong>Music</strong></a></li>
+      <li class="list-group-item text-center"><img class="menu-image2" src="{{ asset('/images/isaac.jpg') }}"height="100" width="120"><img class="menu-image" src="{{ asset('/images/storywalk.jpg') }}" height="100" width="120"><a class="nav-link" href='/places'><strong>Places</strong></a></li>
+      <li class="list-group-item text-center"><img class="menu-image2" src="{{ asset('/images/3rdst.jpg') }}" height="100" width="120"><img class="menu-image" src="{{ asset('/images/local.jpg') }}"height="100" width="120"><a class="nav-link" href='/eatdrink'><strong>Eat & <br>Drink</strong></a></li>
+      <li class="list-group-item text-center"><img class="menu-image2" src="{{ asset('/images/jazzymural.jpg') }}" height="100" width="120"><img class="menu-image" src="{{ asset('/images/shakespeare.jpg') }}" height="100" width="120"><a class="nav-link" href='/local'><strong>Local<br>Business</strong></a></li>
     </ul>
       </div>
   </div>
